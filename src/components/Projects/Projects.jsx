a0dad68f45f4011c8d2f3443af38f778fe43662a
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-06-18 14:42:27 -0500

    add description to project header

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -11,6 +11,7 @@ const Projects = () => {
           <div className="Project">
             <div className="projectHeader">
               <div className="projectTitle">Trackstar</div>
+              <div>Cryptocurrency tracker with customized watchlists</div>
               <div className="refLink">
               <a href="http://3.149.247.65:8080">View in Browser</a>
               <div className="refLinkSpacer"> / </div>
@@ -19,7 +20,6 @@ const Projects = () => {
             </div>
             <img src={trackstar} className="carouselPic" alt="Trackstar" style={{ border: '2px solid black' }}/>
             <ul>
-              <li>Cryptocurrency tracker with customized watchlists</li>
               <li>Single-page React app utilizing AWS cloud PaaS for hosting Postgres database</li>
               <li>Pulls data from coingecko API and facilitates CRUD for watchlist/user management</li>
               <li>Redux Thunk for complex state management</li>
