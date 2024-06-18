4af17a788cd7a027c0f511ae45c2809c1fa917f8
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-06-18 12:05:12 -0500

    feat: start projects page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 import Header from './components/Header'
 import Navbar from './components/Navbar'
-import Projects from './components/Projects'
+import Projects from './components/Projects/Projects'
 import Education from './components/Education'
 import WorkHistory from './components/WorkHistory'
 
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Projects.css'
+
+const Projects = () => {
+  return (
+    <div>
+      <ul className="glide__slides">
+            <li className="glide__slide">
+              <div href="https://github.com/yankee3313">rate repository app</div>
+            </li>
+            <li className="glide__slide">
+              <a href="https://github.com/yankee3313">Trackstar</a>
+            </li>
+            <li className="glide__slide">
+              <a href="https://github.com/yankee3313">TOER Store</a>
+            </li>
+          </ul>
+    </div>
+  )
+}
+
+export default Projects
