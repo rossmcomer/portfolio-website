daadf0e736cef80159ffdba79ab33717b2490b05
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-06-25 23:07:53 -0500

    update projects page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -22,10 +22,12 @@ const Projects = () => {
             <img src={toerstore} className="carouselPic" alt="TOER-store" style={{ border: '2px solid black' }}/>
             <ul>
               <li>Single-page React app utilizing Google Cloud PaaS for hosting Postgres database</li>
+              <li>Containerized via Docker utilizing Nginx for multi-stage build</li>
               <li>Okta/Auth0 PaaS for user management/SSO</li>
               <li>Responsive web design with dark mode feature</li>
               <li>React Context for complex state management</li>
               <li>Node/Express/Axios backend</li>
+              <li>Deployed via Docker Compose on Google Cloud VM using Nginx as reverse-proxy</li>
             </ul>
           </div>
           <div className="Project">
@@ -42,6 +44,7 @@ const Projects = () => {
             <ul>
               <li>Single-page React app utilizing AWS RDS cloud PaaS for hosting Postgres database</li>
               <li>Pulls data from Coingecko API and facilitates CRUD for watchlist/user management</li>
+              <li>Containerized via Docker utilizing Nginx for multi-stage build</li>
               <li>Responsive web design</li>
               <li>Redux Thunk for complex state management</li>
               <li>Node/Express/Axios backend</li>
