2d401d266fcfa5787a1b175eb6aca1b58e671438
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-07-11 21:06:13 -0500

    set width for .carouselContainer

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -13,8 +13,7 @@ const Bio = () => {
                 </div>
                 <div className="summary">
                     <b>Hi,</b> my name is Ross Comer and I am a full-stack developer passionate about creating products that 
-                    meet industry standards yet stand out from the 
-                    competition.  With a background in biomedical engineering and audio engineering
+                    stand out from the competition.  With a background in biomedical engineering and audio engineering
                     I possess a unique set of problem-solving skills,
                     creativity, and technical prowess.  I am seeking an entry-level 
                     software engineering position where I can contribute to innovative projects.
