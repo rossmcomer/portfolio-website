d7d5f7ed73fbbc6e65e588abb059bb57c1ce4ec4
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-07-15 11:28:39 -0500

    add href to projects images

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,54 +8,54 @@ import { Carousel } from 'react-responsive-carousel';
 const Projects = () => {  
         return (
           <div className="carouselContainer">
-          <Carousel showArrows={true} autoPlay infiniteLoop interval={5000} showStatus={false} swipeable={false} showIndicators={false}>
-          <div className="Project">
-            <div className="projectHeader">
-              <div className="projectTitle">TOER Store</div>
-              <div>E-commerce app integrated with Stripe for payments</div>
-              <div className="refLink">
-              <a href="https://toerstore.com">View in Browser</a>
-              <div className="refLinkSpacer"> / </div>
-              <a href="http://github.com/rossmcomer/toer-store">View on Github</a>
+            <Carousel showArrows={true} autoPlay infiniteLoop interval={5000} showStatus={false} swipeable={false} showIndicators={false}>
+            <div className="Project">
+              <div className="projectHeader">
+                <div className="projectTitle">TOER Store</div>
+                <div>E-commerce app integrated with Stripe for payments</div>
+                <div className="refLink">
+                <a href="https://toerstore.com">View in Browser</a>
+                <div className="refLinkSpacer"> / </div>
+                <a href="http://github.com/rossmcomer/toer-store">View on Github</a>
+                </div>
               </div>
+              <a href="https://toerstore.com" className="projectPic">
+                <img src={toerstore} className="carouselPic" alt="TOER-store"/>
+              </a>
+              <ul className="projectDetails">
+                <li>Single-page React app utilizing Google Cloud PaaS for hosting Postgres database</li>
+                <li>Containerized via Docker utilizing Nginx for multi-stage build</li>
+                <li>Okta/Auth0 SaaS for user management/SSO</li>
+                <li>Responsive web design with dark mode feature</li>
+                <li>React Context for complex state management</li>
+                <li>Node/Express/Axios backend</li>
+                <li>Deployed on Google Cloud using Nginx as reverse-proxy</li>
+              </ul>
             </div>
-            <a href="https://toerstore.com">
-            <img src={toerstore} className="carouselPic" alt="TOER-store" style={{ border: '2px solid black' }}/>
-            </a>
-            <ul className="projectDetails">
-              <li>Single-page React app utilizing Google Cloud PaaS for hosting Postgres database</li>
-              <li>Containerized via Docker utilizing Nginx for multi-stage build</li>
-              <li>Okta/Auth0 SaaS for user management/SSO</li>
-              <li>Responsive web design with dark mode feature</li>
-              <li>React Context for complex state management</li>
-              <li>Node/Express/Axios backend</li>
-              <li>Deployed on Google Cloud using Nginx as reverse-proxy</li>
-            </ul>
-          </div>
-          <div className="Project">
-            <div className="projectHeader">
-              <div className="projectTitle">Trackstar</div>
-              <div>Cryptocurrency tracker with customized watchlists</div>
-              <div className="refLink">
-              <a href="http://trackstarapp.com">View in Browser</a>
-              <div className="refLinkSpacer"> / </div>
-              <a href="https://github.com/rossmcomer/trackstar">View on Github</a>
+            <div className="Project">
+              <div className="projectHeader">
+                <div className="projectTitle">Trackstar</div>
+                <div>Cryptocurrency tracker with customized watchlists</div>
+                <div className="refLink">
+                <a href="http://trackstarapp.com">View in Browser</a>
+                <div className="refLinkSpacer"> / </div>
+                <a href="https://github.com/rossmcomer/trackstar">View on Github</a>
+                </div>
               </div>
+              <a href="https://trackstarapp.com" className="projectPic">
+                <img src={trackstar} className="carouselPic" alt="Trackstar"/>
+              </a>
+              <ul className="projectDetails">
+                <li>Single-page React app utilizing AWS RDS cloud PaaS for hosting Postgres database</li>
+                <li>Pulls data from Coingecko API and facilitates CRUD for watchlist/user management</li>
+                <li>Containerized via Docker utilizing Nginx for multi-stage build</li>
+                <li>Redux Thunk for complex state management</li>
+                <li>Node/Express/Axios backend</li>
+                <li>Responsive web design</li>
+                <li>Deployed via AWS EC2 using Nginx as reverse-proxy</li>
+              </ul>
             </div>
-            <a href="https://trackstarapp.com">
-            <img src={trackstar} className="carouselPic" alt="Trackstar" style={{ border: '2px solid black' }}/>
-            </a>
-            <ul className="projectDetails">
-              <li>Single-page React app utilizing AWS RDS cloud PaaS for hosting Postgres database</li>
-              <li>Pulls data from Coingecko API and facilitates CRUD for watchlist/user management</li>
-              <li>Containerized via Docker utilizing Nginx for multi-stage build</li>
-              <li>Redux Thunk for complex state management</li>
-              <li>Node/Express/Axios backend</li>
-              <li>Responsive web design</li>
-              <li>Deployed via AWS EC2 using Nginx as reverse-proxy</li>
-            </ul>
-          </div>
-        </Carousel>
+          </Carousel>
         </div>
         );
 }
