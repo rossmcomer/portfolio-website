857cbba0457de29deb99e0ce36deb6165e7aed31
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-09-28 14:20:46 -0500

    update bio

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -14,7 +14,7 @@ const Bio = () => {
         <div className="summary">
           <b>Hi,</b> my name is Ross Comer and I am a self-motivated developer
           with two years of experience creating full-stack applications.  
-          I possess knowledge of the entire software development lifecycle, 
+          I possess knowledge of the entire software development lifecycle, CI/CD,
           responsive web-design, and continue to learn more each day.  
           I am seeking an entry-level software engineering position where 
           I can apply my passion for solving complex problems as part of a team.
