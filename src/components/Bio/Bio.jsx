a300bea7df63c11a905dbdf30869b0bbb1cb1239
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-10-02 12:24:06 -0500

    update bio

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -3,7 +3,7 @@ import './Bio.css'
 import bioPic from '../../images/bioPicture.jpeg'
 
 const Bio = () => {
-  const text = "Hi, my name is Ross Comer and I am a full-stack developer currently based in Appleton, WI. I used to work with audio but now I work with code. My applications in this portfolio demonstrate my knowledge of the software development lifecycle, continuous integration, and responsive web design."
+  const text = "Hi, my name is Ross Comer and I am a full-stack developer currently based in Appleton, WI. I used to work with audio but now I work with code. My applications in this portfolio demonstrate my knowledge of software development from concept to production."
   const [displayedText, setDisplayedText] = useState("")
   const [index, setIndex] = useState(0)
 
